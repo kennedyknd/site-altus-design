238768115590d4bbbf0d8410c570e27c95f38ca6
Kennedy Alves <kennedyalves.ka@gmail.com>
2022-05-24 15:18:57 -0300

    Adicionado Slider

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+$(function(){
+
+  $('.autoplay').slick({
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 1400,
+        settings: {
+          slidesToShow: 3
+        }
+      }
+    ]
+  });
+
+  $('.autoplay2').slick({
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
+
+})
