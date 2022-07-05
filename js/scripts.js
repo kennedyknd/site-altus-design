cdea9484d1c074f92b0a2468784f075c814501aa
Kennedy Alves <kennedyalves.ka@gmail.com>
2022-07-05 19:47:31 -0300

    Adicionado script js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 
 $(function(){
 
+  window.onload = function(e) {
+    var offset = document.getElementsByClassName('header')[0].offsetTop;
+    var menu = document.getElementsByClassName('header')[0];
+
+    document.addEventListener('scroll', function() {
+        if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
+            menu.style.position = 'fixed';
+        } else {
+            menu.style.position = 'initial';
+        }
+    });
+}
+
   $('.autoplay').slick({
     dots: true,
     slidesToShow: 4,
